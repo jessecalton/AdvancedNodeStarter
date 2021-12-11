1dcd3249c073ec1ce06b9e73e92b7de799aa8563
jessecalton <jcalton@dotdash.com>
2021-12-10 17:16:21 -0800

    first node tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,28 @@
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
+const puppeteer = require('puppeteer');
 
-    expect(sum).toEqual(3);
+let browser, page; // scoping the variables so they are accessible in each func
+
+beforeEach( async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+})
+
+afterEach( async () => {
+    await browser.close();
 });
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML); // nothing special about the $ sign.
+    expect(text).toEqual('Blogster');
+})
+
+test('clicking login starts the oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+})
