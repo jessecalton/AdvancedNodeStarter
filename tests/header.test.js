85719e43eac5c835cf17b7c9f9e5964584e5f785
jessecalton <jcalton@dotdash.com>
2021-12-16 17:13:42 -0800

    more test

--- tests/header.test.js
+++ tests/header.test.js
@@ -26,3 +26,32 @@ test('clicking login starts the oauth flow', async () => {
 
     expect(url).toMatch(/accounts\.google\.com/);
 })
+
+test.only('when signed in shows logout button', async () => {
+    const id = '61ba845cd63687174d1c9212'; // copied from MongoDB collection
+    
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+    const sessionString = Buffer.from(JSON.stringify(sessionObject))
+        .toString('base64'); // sessionString will be the exact same session string as what we'd get using keygrip
+    
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString); // 'session=' is just what the keygrip library does. No other reason.
+    
+    await page.setCookie({ name: 'session', value: sessionString});
+    await page.setCookie({ name: 'session.sig', value: sig});
+    // Can get your cookie names in Chrome inspector --> application --> Cookies --> localhost:3000, and look under "Name" heading
+
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]'); // test will fail if <a> tag never appears
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
+});
